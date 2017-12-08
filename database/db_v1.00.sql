79834f9f754b31e73abc911099572025d361ab26
JanickiD <janicki.damian@wp.pl>
2017-12-08 15:36:50 +0100

    edit sql for create database (db_v1.00.sql)

--- database/db_v1.00.sql
+++ database/db_v1.00.sql
@@ -20,8 +20,10 @@ USE `almma` ;
 -- -----------------------------------------------------
 -- Table `almma`.`category`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `almma`.`category` ;
+
 CREATE TABLE IF NOT EXISTS `almma`.`category` (
-  `id_cat` INT NOT NULL,
+  `id_cat` INT(11) NOT NULL,
   `name_cat` VARCHAR(30) NULL DEFAULT NULL,
   PRIMARY KEY (`id_cat`),
   UNIQUE INDEX `id_cat` (`id_cat` ASC))
@@ -32,26 +34,29 @@ DEFAULT CHARACTER SET = utf8;
 -- -----------------------------------------------------
 -- Table `almma`.`club`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `almma`.`club` ;
+
 CREATE TABLE IF NOT EXISTS `almma`.`club` (
   `id_club` INT(4) NOT NULL AUTO_INCREMENT,
-  `name_club` VARCHAR(30) NOT NULL,
-  `city_club` VARCHAR(25) NULL DEFAULT NULL,
-  `trainer_name` VARCHAR(40) NULL DEFAULT NULL,
-  PRIMARY KEY (`id_club`),
-  UNIQUE INDEX `id_club` (`id_club` ASC))
+  `name_club` VARCHAR(50) NULL DEFAULT NULL,
+  `city_club` VARCHAR(25) NULL DEFAULT 'Miasto',
+  `trainer_name` VARCHAR(40) NULL DEFAULT 'Nazwisko Trenera',
+  PRIMARY KEY (`id_club`))
 ENGINE = InnoDB
-AUTO_INCREMENT = 4
+AUTO_INCREMENT = 84
 DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
 -- Table `almma`.`weight_cat`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `almma`.`weight_cat` ;
+
 CREATE TABLE IF NOT EXISTS `almma`.`weight_cat` (
-  `id_weight` INT NOT NULL,
+  `id_weight` INT(11) NOT NULL,
   `value_weight` VARCHAR(4) NOT NULL,
-  UNIQUE INDEX `value_weight` (`value_weight` ASC),
-  PRIMARY KEY (`id_weight`))
+  PRIMARY KEY (`id_weight`),
+  UNIQUE INDEX `value_weight` (`value_weight` ASC))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
@@ -59,57 +64,101 @@ DEFAULT CHARACTER SET = utf8;
 -- -----------------------------------------------------
 -- Table `almma`.`player`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `almma`.`player` ;
+
 CREATE TABLE IF NOT EXISTS `almma`.`player` (
   `id_p` INT(3) NOT NULL AUTO_INCREMENT,
   `name_p` VARCHAR(25) NOT NULL,
   `last_name_p` VARCHAR(40) NOT NULL,
-  `weight` INT NOT NULL,
+  `id_weight` INT(11) NOT NULL,
   `id_club` INT(3) NOT NULL,
-  `id_cat` INT NOT NULL,
-  PRIMARY KEY (`id_p`, `id_cat`, `id_club`, `weight`),
-  UNIQUE INDEX `id_p` (`id_p` ASC),
+  PRIMARY KEY (`id_p`, `id_club`, `id_weight`),
   INDEX `fk_player_club1_idx` (`id_club` ASC),
-  INDEX `fk_player_category1_idx` (`id_cat` ASC),
-  INDEX `fk_player_weight_cat1_idx` (`weight` ASC),
+  INDEX `fk_player_weight_cat1_idx` (`id_weight` ASC),
+  UNIQUE INDEX `id_p_UNIQUE` (`id_p` ASC),
   CONSTRAINT `fk_player_club1`
     FOREIGN KEY (`id_club`)
     REFERENCES `almma`.`club` (`id_club`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_player_category1`
-    FOREIGN KEY (`id_cat`)
-    REFERENCES `almma`.`category` (`id_cat`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
   CONSTRAINT `fk_player_weight1`
-    FOREIGN KEY (`weight`)
+    FOREIGN KEY (`id_weight`)
     REFERENCES `almma`.`weight_cat` (`id_weight`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
-AUTO_INCREMENT = 12
+AUTO_INCREMENT = 2
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `almma`.`fight_field`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `almma`.`fight_field` ;
+
+CREATE TABLE IF NOT EXISTS `almma`.`fight_field` (
+  `id_fight_field` INT(11) NOT NULL,
+  `fight_field_name` VARCHAR(25) NOT NULL,
+  PRIMARY KEY (`id_fight_field`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `almma`.`fight`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `almma`.`fight` ;
+
+CREATE TABLE IF NOT EXISTS `almma`.`fight` (
+  `id_fight` INT(11) NOT NULL AUTO_INCREMENT,
+  `id_p_blue` INT(11) NOT NULL,
+  `id_p_red` INT(11) NOT NULL,
+  `fight_field_id_fight_field` INT(11) NOT NULL,
+  PRIMARY KEY (`id_fight`),
+  INDEX `fk_fight_player1_idx` (`id_p_blue` ASC),
+  INDEX `fk_fight_player2_idx` (`id_p_red` ASC),
+  INDEX `fk_pole_walki_idx` (`fight_field_id_fight_field` ASC),
+  CONSTRAINT `fk_fight_player1`
+    FOREIGN KEY (`id_p_blue`)
+    REFERENCES `almma`.`player` (`id_p`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_fight_player2`
+    FOREIGN KEY (`id_p_red`)
+    REFERENCES `almma`.`player` (`id_p`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_pole_walki`
+    FOREIGN KEY (`fight_field_id_fight_field`)
+    REFERENCES `almma`.`fight_field` (`id_fight_field`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
 -- Table `almma`.`tournament`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `almma`.`tournament` ;
+
 CREATE TABLE IF NOT EXISTS `almma`.`tournament` (
   `id_tourn` INT(4) NOT NULL AUTO_INCREMENT,
-  `name_tourn` VARCHAR(30) NULL,
+  `name_tourn` VARCHAR(30) NULL DEFAULT NULL,
   `city_tourn` VARCHAR(25) NOT NULL,
   `date_tourn` DATE NOT NULL,
   `rank_tourn` INT(11) NOT NULL,
   PRIMARY KEY (`id_tourn`),
   UNIQUE INDEX `id_tourn` (`id_tourn` ASC))
 ENGINE = InnoDB
-AUTO_INCREMENT = 4
 DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
 -- Table `almma`.`rank`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `almma`.`rank` ;
+
 CREATE TABLE IF NOT EXISTS `almma`.`rank` (
   `id_rank` INT(1) NOT NULL AUTO_INCREMENT,
   `rank` VARCHAR(25) NOT NULL,
@@ -123,29 +172,37 @@ CREATE TABLE IF NOT EXISTS `almma`.`rank` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
-AUTO_INCREMENT = 4
 DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
 -- Table `almma`.`results`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `almma`.`results` ;
+
 CREATE TABLE IF NOT EXISTS `almma`.`results` (
-  `id_result` INT NOT NULL AUTO_INCREMENT,
-  `id_player` INT(3) NOT NULL,
-  `id_tourn` INT(4) NOT NULL,
-  `place` INT(1) NULL,
-  PRIMARY KEY (`id_result`, `id_player`, `id_tourn`),
+  `id_result` INT(11) NOT NULL AUTO_INCREMENT,
+  `id_player` INT(3) NOT NULL DEFAULT 0,
+  `id_tourn` INT(4) NOT NULL DEFAULT 0,
+  `place` INT(1) NULL DEFAULT NULL,
+  `id_cat` INT(11) NOT NULL,
+  PRIMARY KEY (`id_result`, `id_player`, `id_tourn`, `id_cat`),
   INDEX `fk_results_tournament1_idx` (`id_tourn` ASC),
   INDEX `fk_results_player1_idx` (`id_player` ASC),
+  INDEX `fk_results_category1_idx` (`id_cat` ASC),
+  CONSTRAINT `fk_results_player1`
+    FOREIGN KEY (`id_player`)
+    REFERENCES `almma`.`player` (`id_p`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
   CONSTRAINT `fk_results_tournament1`
     FOREIGN KEY (`id_tourn`)
     REFERENCES `almma`.`tournament` (`id_tourn`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_results_player1`
-    FOREIGN KEY (`id_player`)
-    REFERENCES `almma`.`player` (`id_p`)
+  CONSTRAINT `fk_results_category1`
+    FOREIGN KEY (`id_cat`)
+    REFERENCES `almma`.`category` (`id_cat`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
@@ -153,44 +210,43 @@ DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `almma`.`fight_field`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `almma`.`fight_field` (
-  `id_fight_field` INT NOT NULL,
-  `fight_field_name` VARCHAR(25) NULL,
-  `fight_field_fight_no` INT NULL,
-  PRIMARY KEY (`id_fight_field`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `almma`.`fight`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `almma`.`fight` (
-  `id_fight` INT NOT NULL AUTO_INCREMENT,
-  `id_p_blue` INT NOT NULL,
-  `id_p_red` INT NOT NULL,
-  `fight_field_id_fight_field` INT NOT NULL,
-  PRIMARY KEY (`id_fight`, `fight_field_id_fight_field`),
-  INDEX `fk_fight_fight_field1_idx` (`fight_field_id_fight_field` ASC),
-  CONSTRAINT `fk_fight_fight_field1`
-    FOREIGN KEY (`fight_field_id_fight_field`)
-    REFERENCES `almma`.`fight_field` (`id_fight_field`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
 -- Table `almma`.`user`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `almma`.`user` ;
+
 CREATE TABLE IF NOT EXISTS `almma`.`user` (
-  `id_user` INT NOT NULL,
+  `id_user` INT(11) NOT NULL,
   `login` VARCHAR(45) NOT NULL,
-  `user_group` INT NOT NULL,
+  `user_group` INT(11) NOT NULL,
   `pass` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id_user`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `almma`.`category_has_player`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `almma`.`category_has_player` ;
+
+CREATE TABLE IF NOT EXISTS `almma`.`category_has_player` (
+  `category_id_cat` INT(11) NOT NULL,
+  `player_id_p` INT(3) NOT NULL,
+  PRIMARY KEY (`category_id_cat`, `player_id_p`),
+  INDEX `fk_category_has_player_player1_idx` (`player_id_p` ASC),
+  INDEX `fk_category_has_player_category1_idx` (`category_id_cat` ASC),
+  CONSTRAINT `fk_category_has_player_category1`
+    FOREIGN KEY (`category_id_cat`)
+    REFERENCES `almma`.`category` (`id_cat`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_category_has_player_player1`
+    FOREIGN KEY (`player_id_p`)
+    REFERENCES `almma`.`player` (`id_p`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
