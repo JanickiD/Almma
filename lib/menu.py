1d752ec36a6b1fe9653cbc0205268a440c106e2d
JanickiD <janicki.damian@wp.pl>
2017-12-13 15:40:06 +0100

    2

--- lib/menu.py
+++ lib/menu.py
@@ -10,6 +10,7 @@ def menuChoice():
     return ch
 
 def showMainMenu(userGroup):
+    os.system('cls')
     if userGroup == 1:
         m = {1: "Menu Administratora", 2:"Pokaż drzewka", 3:"Zawodnicy", 4:"Koniec"}
         for i in m:
@@ -23,8 +24,15 @@ def showMenuAdmin():
     for i in menu:
         print("[",j,"]",menu[i])
         j +=1
-
-
+def showMenuTournamentsManagement():
+    os.system('cls')
+    print('##########   Zarządzanie zawodami   ###############')
+    menu = {1:"Pokaż otwarte zawody", 2:"Dodaj zawody", 3:"Zmień status", 4:"Powrót"}
+    j = 1
+    for i in menu:
+        print("[",j,"]",menu[i])
+        j +=1
+        
 def menuAdmin():
     statusmA = 0
     while statusmA != 99:
@@ -33,7 +41,7 @@ def menuAdmin():
         if choice == 1:
             menuqNumberInCat()
         elif choice == 2:
-            pass
+            menuTournamentsManagement()
         elif choice == 3:
             pass
         elif choice == 4:
@@ -47,8 +55,47 @@ def menuqNumberInCat():
         zapytania.qNumberInCat(connPar)
         choiceqNIC = menuChoice()
         if choiceqNIC == 1:
-            pass
+            menuqPlayersInWeightCat()
         elif choiceqNIC == 2:
             break
         else:
             print("Nieprawidłowy wybór")
+
+def menuqPlayersInWeightCat():
+    statusqPIWC = 0
+    while statusqPIWC != 99:
+        zapytania.qPlayersInWeightCat(connPar)
+        chocieqPIWC = menuChoice()
+        if chocieqPIWC != None:
+            break
+        else:
+            print("Nieprawidłowy wybór")
+            
+def menuTournamentsManagement():
+    statusmTM = 0
+    while statusmTM != 99:
+        showMenuTournamentsManagement()
+        choiceTM = menuChoice()
+        print(choiceTM)
+        if choiceTM == 1:
+            menuShowAllTournaments()
+        elif choiceTM == 2:
+            pass
+        elif choiceTM == 3:
+            pass
+        elif choiceTM == 4:
+            break
+        else:
+            print("Nieprawidłowy wybór")
+            
+def menuShowAllTournaments():
+    statusmSAT = 0
+    while statusmSAT != 99:
+        zapytania.qShowAllTournaments(connPar)
+        choicemSAT = menuChoice()
+        if choicemSAT == 1:
+            pass
+        elif choicemSAT == 2:
+            break
+        else:
+            print("Wybór niepoprawny")
